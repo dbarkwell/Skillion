8fca8d71c561ca5473aa46393765f1c8ae9e9b51
Dan Barkwell <dbarkwell@pelism.com>
2020-05-11 16:07:41 -0400

    Fix example project

--- examples/SimpleIntent/Startup.cs
+++ examples/SimpleIntent/Startup.cs
@@ -20,7 +20,7 @@ namespace SimpleIntent
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddSkillion();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
